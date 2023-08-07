e10dfb6de211b3afb5cf8e7a1ae8e85b09b6a6af
endesnicolascondori <127762793+endesnicolascondori@users.noreply.github.com>
2023-08-06 22:53:31 -0500

    compartido appp

--- service_worker.js
+++ service_worker.js
@@ -1,52 +1,52 @@
-var CACHENAME = "cachestore";
-var FILES = [
-  "/img/male.png",
-  "/banner1.JPG",
-  "/index.html",
-  "/recursos/flow.css",
-  "/recursos/filesaver.js",
-  "/recursos/flow.js",
-  "/recursos/theme.js",
-  "/recursos/jszip.js",
-  "/recursos/xml.js"
-];
-//Cached core static resources 
-self.addEventListener("install",e=>{
-    e.waitUntil(
-      caches.open(CACHENAME).then(cache=>{
-        return cache.addAll(FILES);
-      })
-    );
-  });
+// var CACHENAME = "cachestore";
+// var FILES = [
+//   "/img/male.png",
+//   "/banner1.JPG",
+//   "/index.html",
+//   "/recursos/flow.css",
+//   "/recursos/filesaver.js",
+//   "/recursos/flow.js",
+//   "/recursos/theme.js",
+//   "/recursos/jszip.js",
+//   "/recursos/xml.js"
+// ];
+// //Cached core static resources 
+// self.addEventListener("install",e=>{
+//     e.waitUntil(
+//       caches.open(CACHENAME).then(cache=>{
+//         return cache.addAll(FILES);
+//       })
+//     );
+//   });
   
-  // Fatch resources
-  self.addEventListener("fetch",e=>{
-    e.respondWith(
-      caches.match(e.request).then(response=>{
-        return response||fetch(e.request);
-      })
-    );
-  });
+//   // Fatch resources
+//   self.addEventListener("fetch",e=>{
+//     e.respondWith(
+//       caches.match(e.request).then(response=>{
+//         return response||fetch(e.request);
+//       })
+//     );
+//   });
 
 
-// // Cached core static resources
-// function cacheCoreStaticResources() {
-//   const cache = caches.open(CACHENAME);
-//   for (const file of FILES) {
-//     cache.put(file);
-//   }
-// }
+// // // Cached core static resources
+// // function cacheCoreStaticResources() {
+// //   const cache = caches.open(CACHENAME);
+// //   for (const file of FILES) {
+// //     cache.put(file);
+// //   }
+// // }
 
-// self.addEventListener("install", event => {
-//   event.waitUntil(cacheCoreStaticResources());
-// });
+// // self.addEventListener("install", event => {
+// //   event.waitUntil(cacheCoreStaticResources());
+// // });
 
-// // Fetch resources
-// function fetchResource(request) {
-//   const response = caches.match(request);
-//   return response || fetch(request);
-// }
+// // // Fetch resources
+// // function fetchResource(request) {
+// //   const response = caches.match(request);
+// //   return response || fetch(request);
+// // }
 
-// self.addEventListener("fetch", event => {
-//   event.respondWith(fetchResource(event.request));
-// });
+// // self.addEventListener("fetch", event => {
+// //   event.respondWith(fetchResource(event.request));
+// // });
